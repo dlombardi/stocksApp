05adc1b1aeb8e6b010f0dadb91624aedc71106cf
Sanomi <snrutledge@gmail.com>
2015-09-17 23:52:39 -0700

    add omplete functionality

--- Front-End/main.js
+++ Front-End/main.js
@@ -28,37 +28,40 @@ let stockApp = angular.module('stockApp', ['ui.router'])
 
 stockApp.controller( 'listQuotesController', function($scope, tracked, displayStocks) {
   let quote = [];
-  tracked.read()
-  .then(function(res) {
-    console.log('res:', res.data);
+  $scope.stockList = quote;
+  
+  tracked.read(function(res) {
+    // console.log('res:', res);
     $scope.trackedStocks = res.data;
-    displayStocks.listBuilder(res.data);
+    displayStocks.listBuilder(res.data, function(data) {
+      // console.log(data);
+      // console.log(data.Symbol);
+      // console.log(data.LastPrice);
+      quote.push({name: data.Symbol, price: data.LastPrice});
+      console.log(quote)
 
+    });
   })
-    .catch(function(e) {
-      console.log('error', e)
-    })
 })
 
 stockApp.service("displayStocks", function($http){
   
-  this.listBuilder = function(stockArr){
+  this.listBuilder = function(stockArr, cb){
     for (let i of stockArr){
-    console.log(this.getStocks(i));
+    this.getStocks(i , cb);
    }
 
   }
-  this.getStocks = function(symbol){
+  this.getStocks = function(symbol, cb){
      return  $http.jsonp(`http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=${symbol}&callback=JSON_CALLBACK`)
      .success(function(data) {
-      console.log(data);
-      return data;
-        // quote.push({name: data.data.Symbol, price: data.data.LastPrice});
-        // console.log(quote);
+      cb(data);
       }) ; 
   }
-});
 
+
+
+});
 
 
 stockApp.controller('mainController', function($scope){
@@ -68,28 +71,20 @@ stockApp.controller('addStockController', function($scope, stockService, tracked
 
   $scope.populateTextArea = function(stockItem) {
     var symbol = stockItem.Symbol;
-    console.log(symbol);
     $scope.company = symbol;
   }
   $scope.trackInput = function(){
     stockService.getStocks($scope.company)
     .success(function(data){
       $scope.stockList = data;
-      console.log($scope.stockList);
     });
   }
-  tracked.read()
-  .then(function(res) {
-    console.log('res:', res.data);
+  tracked.read(function(res) {
     $scope.trackedStocks = res.data;
   })
-    .catch(function(e) {
-      console.log('error', e)
-    })
   $scope.addTracked = function() {
     tracked.add($scope.company)
     .then(function(data) {
-      console.log('data', data.data);
       $scope.trackedStocks = data.data;
     })
   }
@@ -103,12 +98,13 @@ stockApp.service("stockService", function($http){
 
 stockApp.factory('tracked', function($http) {
   var tracked = function() {};
-    tracked.read = function() {
-      return $http.get('http://localhost:3000/tracked')
+    tracked.read = function(cb) {
+      $http.get('http://localhost:3000/tracked')
+      .then(function(res) { 
+        cb(res)});
     };
     
     tracked.add =function(symbol) {
-      console.log(symbol);
       return $http.post('http://localhost:3000/tracked', {newSymbol: symbol})
     }
   return tracked;
